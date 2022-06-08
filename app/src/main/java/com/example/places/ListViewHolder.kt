1a42e2fa5a4611e57e0a91d0040e24cb4f4bdff8
EJM5 <49279528+EJM5@users.noreply.github.com>
2022-06-08 09:31:28 +0300

    Revert "Feature/code cleanup"

--- app/src/main/java/com/example/places/ListViewHolder.kt
+++ app/src/main/java/com/example/places/ListViewHolder.kt
@@ -5,19 +5,23 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 
-class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ListViewHolder(itemView :View) : RecyclerView.ViewHolder(itemView) {
     private val tvName = itemView.findViewById<TextView>(R.id.name_text_view)
     private val tvAddress = itemView.findViewById<TextView>(R.id.address_text_view)
     private val ivPlace = itemView.findViewById<ImageView>(R.id.place_image_view)
-    private val tvEmail = itemView.findViewById<TextView>(R.id.email_address_text_view)
+    private val tvEmail = itemView.findViewById<TextView>(R.id.emailaddres_text_view)
     private val tvDescription = itemView.findViewById<TextView>(R.id.description_text_view)
-    private val tvTel = itemView.findViewById<TextView>(R.id.tel_text_view)
+
 
     fun setItem(place: Place) {
         tvName.text = place.name
         tvAddress.text = place.address
         tvEmail.text = place.email
         tvDescription.text = place.description
-        tvTel.text = place.tel
+
+
+
     }
+
+
 }
