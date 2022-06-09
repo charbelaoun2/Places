aed9514db2093aa99a8adb3546560d1e560702a5
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2022-06-09 16:40:34 +0300

    remove useless import and the name attribute

--- app/src/main/java/com/example/places/MapFragment.kt
+++ app/src/main/java/com/example/places/MapFragment.kt
@@ -8,11 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.places.databinding.MapFragmentBinding
 import com.example.places.viewmodels.PlacesViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(R.layout.map_fragment), OnMapReadyCallback {
     private var binding: MapFragmentBinding? = null
@@ -40,7 +37,7 @@ class MapFragment : Fragment(R.layout.map_fragment), OnMapReadyCallback {
     override fun onMapReady(p0: GoogleMap) {
         p0.let {
             googleMap = it
-           
+
         }
     }
 
--- app/src/main/java/com/example/places/PlaceApi.kt
+++ app/src/main/java/com/example/places/PlaceApi.kt
@@ -1,6 +1,5 @@
 package com.example.places
 
-import androidx.lifecycle.MutableLiveData
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
