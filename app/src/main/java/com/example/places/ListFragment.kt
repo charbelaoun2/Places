d7423a5a43fb355bff194f23af8b16ca0b935ee7
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2022-07-05 17:29:42 +0300

    add the swipe design with the icon

--- app/src/main/java/com/example/places/ListFragment.kt
+++ app/src/main/java/com/example/places/ListFragment.kt
@@ -1,13 +1,20 @@
 package com.example.places
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.places.databinding.LayoutFilterBinding
 import com.example.places.databinding.ListFragmentBinding
 import com.example.places.databinding.PlaceDetailsBinding
@@ -22,6 +29,8 @@ class ListFragment : Fragment(R.layout.list_fragment), ListAdapter.OnItemClickLi
     private var binding: ListFragmentBinding? = null
     private lateinit var binding3: LayoutFilterBinding
     private lateinit var binding2: PlaceDetailsBinding
+    private var swipeBackground : ColorDrawable = ColorDrawable(Color.parseColor("#0371C9"))
+    private lateinit var saveIcon : Drawable
     private lateinit var listAdapter: ListAdapter
     private lateinit var firebaseAnalytics: FirebaseAnalytics
     private val viewModel by activityViewModels<PlacesViewModel>()
@@ -40,6 +49,7 @@ class ListFragment : Fragment(R.layout.list_fragment), ListAdapter.OnItemClickLi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAnalytics = Firebase.analytics
+        saveIcon = ContextCompat.getDrawable(requireContext(),R.drawable.ic_baseline_save_24)!!
         viewModel.placesLiveData.observe(viewLifecycleOwner) { places ->
             setupAdapter(places)
             updateRecyclerView(places)
@@ -78,9 +88,55 @@ class ListFragment : Fragment(R.layout.list_fragment), ListAdapter.OnItemClickLi
     }
 
     private fun setupAdapter(placesList: List<Place>) {
+
         listAdapter = ListAdapter(
             placesList, this
         )
+        val swipeToSaveCallback = object : SwipeGesture() {
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                viewHolder.adapterPosition
+                listAdapter.notifyItemChanged(viewHolder.adapterPosition)
+            }
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                val iconMargin = (itemView.height - saveIcon.intrinsicHeight) /2
+
+
+                if (dX > 0) {
+                    swipeBackground.setBounds(itemView.left,itemView.top,dX.toInt(),itemView.bottom)
+                    saveIcon.setBounds(itemView.left + iconMargin,itemView.top + iconMargin,
+                    itemView.left + iconMargin + saveIcon.intrinsicWidth,itemView.bottom - iconMargin)
+                } else {
+                    swipeBackground.setBounds(itemView.right+dX.toInt(),itemView.top,itemView.right,itemView.bottom)
+                    saveIcon.setBounds(itemView.right - iconMargin - saveIcon.intrinsicWidth,itemView.top + iconMargin,
+                        itemView.right - iconMargin ,itemView.bottom - iconMargin)
+                }
+                swipeBackground.draw(c)
+                if ( dX >0 ) {
+                    c.clipRect(itemView.left,itemView.top,dX.toInt(),itemView.bottom)
+                } else {
+                    c.clipRect(itemView.right+dX.toInt(),itemView.top,itemView.right,itemView.bottom)
+                }
+                saveIcon.draw(c)
+                c.restore()
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+            }
+        }
+
+
+        val itemTouchHelper = ItemTouchHelper(swipeToSaveCallback)
+        itemTouchHelper.attachToRecyclerView(binding?.placesRecyclerView)
 
         binding?.placesRecyclerView?.adapter = listAdapter
         binding?.placesRecyclerView?.layoutManager = LinearLayoutManager(context)
