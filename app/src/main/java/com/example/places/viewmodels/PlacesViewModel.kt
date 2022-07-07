4e12317411d2cb23f9b77a4cd6e2427438b084db
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2022-07-07 15:39:38 +0300

    Code cleaning\

--- app/src/main/java/com/example/places/viewmodels/PlacesViewModel.kt
+++ app/src/main/java/com/example/places/viewmodels/PlacesViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.places.viewmodels
 
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
